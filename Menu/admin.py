dfed83d5b7e3ea42d3ab42bc4633740d0e31bfe6
Sourav Ganguly <95974201+isouravganguly@users.noreply.github.com>
2022-04-05 15:16:32 -0700

    Update admin.py

--- Menu/admin.py
+++ Menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import department, employees, products, transactions
+
 # Register your models here.
+admin.site.register(book)
+admin.site.register(librarian)
